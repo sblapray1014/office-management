5c383ef1408bb366defb27b97be44af18bdc568a
camjaywilson <camjaywilson@gmail.com>
2019-05-17 22:47:18 -0600

    Task and template merge on task get route

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,84 +4,76 @@ const { check, validationResult } = require("express-validator/check");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-<<<<<<< HEAD
 const config = require('config');
 const User = require('../../models/User');
 const auth = require('../../middleware/auth');
-=======
-const config = require("config");
-const User = require("../../models/User");
->>>>>>> master
 
 // @route   POST api/users
 // @desc    register user
 // @access  Public
 router.post(
-  "/",
-  [
-    check("name", "Name is required")
-      .not()
-      .isEmpty(),
-    check("email", "Please include a valid email").isEmail(),
-    check(
-      "password",
-      "Please enter a password with 6 or more characters"
-    ).isLength({ min: 6 })
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { name, email, phone, password } = req.body;
-
-    try {
-      let user = await User.findOne({ email });
-
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ message: "User already exists" }] });
-      }
-
-      user = new User({
-        name,
-        email,
-        phone,
-        password
-      });
-
-      const salt = await bcrypt.genSalt(10);
-
-      user.password = await bcrypt.hash(password, salt);
-
-      await user.save();
-
-      const payload = {
-        user: {
-          id: user.id
+    "/",
+    [
+        check("name", "Name is required")
+            .not()
+            .isEmpty(),
+        check("email", "Please include a valid email").isEmail(),
+        check(
+            "password",
+            "Please enter a password with 6 or more characters"
+        ).isLength({ min: 6 })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-      };
 
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: 2160000 }, // <---------- UPDATE IN PRODUCTION CODE
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
+        const { name, email, phone, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ message: "User already exists" }] });
+            }
+
+            user = new User({
+                name,
+                email,
+                phone,
+                password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get("jwtSecret"),
+                { expiresIn: 2160000 }, // <---------- UPDATE IN PRODUCTION CODE
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (error) {
+            console.log("Error: " + error.message);
+            res.status(500).send("Server Error");
         }
-      );
-    } catch (error) {
-      console.log("Error: " + error.message);
-      res.status(500).send("Server Error");
     }
-  }
 );
-
-<<<<<<< HEAD
-});
 
 // @route   POST api/users
 // @desc    get user logged in user
@@ -106,6 +98,3 @@ router.get('/', auth, async (req, res) => {
 // @access  Private
 
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> master
